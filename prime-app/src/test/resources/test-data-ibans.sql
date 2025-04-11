5fe9fd6bad212afc297ccb08ff91e8d35c507f47
Stanislav Olshanskyi <stasolsh77@gmail.ccom>
2025-04-11 21:06:42 +0300

    add tests

---
+++ prime-app/src/test/resources/test-data-ibans.sql
@@ -0,0 +1,5 @@
+
+insert into BlacklistedIban(id, IBAN_CODE) values(1, 'DE15300606010505780780');
+insert into BlacklistedIban(id, IBAN_CODE) values(2, 'UPC82771401621500311');
+insert into BlacklistedIban(id, IBAN_CODE) values(3, 'UPC82771401621500311');
+
