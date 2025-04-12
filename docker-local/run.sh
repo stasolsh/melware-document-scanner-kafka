f1b11ce0acd89d25e7a6924a5a7f6fb6b2dda7d2
Stanislav Olshanskyi <stasolsh77@gmail.ccom>
2025-04-12 21:41:56 +0300

    follow to proper naming

--- docker-local/run.sh
+++ docker-local/run.sh
@@ -8,10 +8,10 @@ ROOT=$(pwd)
 
 docker-compose -f docker-compose.yml stop
 
-cd "$ROOT"/melware-document-scanner-kafka/test-data-producer
+cd "$ROOT"/malware-document-scanner-kafka/test-data-producer
 mvn -T 1C clean install -DskipTests=true
 
-cd "$ROOT"/melware-document-scanner-kafka/prime-app
+cd "$ROOT"/malware-document-scanner-kafka/prime-app
 mvn -T 1C clean install -DskipTests=true
 
 cd "$ROOT"/
